3f8db8489dab66698eeb8662e82e26184bfffc0a
arnedeBo <arne.deborman@web.de>
2020-12-18 09:20:51 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const token = process.env.BOT_TOKEN;
+bot.login(token);
+bot.on('guildMemberAdd', async member => {
+	console.log(member);
+	let connection = await member.voice.channel.join();
+	connection.play('./Hallo.mp3');
+});
+bot.on('message', async msg => {
+	console.log(msg);
+	if (msg.content.toLowerCase() == 'hallo') {
+		if (msg.member.voice.channel) {
+			let connection = await msg.member.voice.channel.join();
+			connection.play('./Hallo.mp3');
+		}
+	}
+});
+bot.on('ready', () => {
+	console.log('ready');
+});
